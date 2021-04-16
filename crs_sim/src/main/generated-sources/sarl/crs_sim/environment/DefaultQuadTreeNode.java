fde9159e56e493f832f7b1f168791730e0fa16c0
Twomens <55756182+Twomens@users.noreply.github.com>
2021-04-16 19:58:07 +0200

    final push

---
+++ crs_sim/src/main/generated-sources/sarl/crs_sim/environment/DefaultQuadTreeNode.java
@@ -0,0 +1,53 @@
+package crs_sim.environment;
+
+import crs_sim.body.EnvObject;
+import io.sarl.lang.annotation.SarlElementType;
+import io.sarl.lang.annotation.SarlSpecification;
+import io.sarl.lang.annotation.SyntheticMember;
+import java.util.Collection;
+import org.arakhne.afc.math.geometry.d2.d.Rectangle2d;
+import org.arakhne.afc.math.tree.node.QuadTreeNode;
+import org.eclipse.xtext.xbase.lib.Pure;
+
+/**
+ * @author Thomas
+ */
+@SarlSpecification("0.11")
+@SarlElementType(10)
+@SuppressWarnings("all")
+public class DefaultQuadTreeNode extends QuadTreeNode<EnvObject, DefaultQuadTreeNode> {
+  private Rectangle2d area;
+  
+  public DefaultQuadTreeNode() {
+    super();
+  }
+  
+  /**
+   * Construct a node.
+   * @param data are the initial user data
+   */
+  public DefaultQuadTreeNode(final Collection<EnvObject> data) {
+    super(data);
+  }
+  
+  public void insert(final EnvObject object) {
+  }
+  
+  @Override
+  @Pure
+  @SyntheticMember
+  public boolean equals(final Object obj) {
+    return super.equals(obj);
+  }
+  
+  @Override
+  @Pure
+  @SyntheticMember
+  public int hashCode() {
+    int result = super.hashCode();
+    return result;
+  }
+  
+  @SyntheticMember
+  private static final long serialVersionUID = -1893959734L;
+}
---
+++ crs_sim/src/main/generated-sources/sarl/crs_sim/utils/DefaultTreeBuilder.java
@@ -0,0 +1,15 @@
+package crs_sim.utils;
+
+import io.sarl.lang.annotation.SarlElementType;
+import io.sarl.lang.annotation.SarlSpecification;
+
+/**
+ * @author Thomas
+ */
+@SarlSpecification("0.11")
+@SarlElementType(10)
+@SuppressWarnings("all")
+public class DefaultTreeBuilder {
+  private DefaultTreeBuilder() {
+  }
+}
