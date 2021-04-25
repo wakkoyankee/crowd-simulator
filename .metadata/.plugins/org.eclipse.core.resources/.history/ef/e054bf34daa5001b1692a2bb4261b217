ad2e007d3fd1dfc694783bfb1abadeaa6ed98f44
Twomens <55756182+Twomens@users.noreply.github.com>
2021-04-25 18:09:15 +0200

    I tried

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/ef/e054bf34daa5001b1692a2bb4261b217
@@ -0,0 +1,6 @@
+package crs_sim.utils;
+
+public class ParamSimu {
+	public static final double mapSizeX = 1000;
+	public static final double mapSizeY = 1000;
+}
--- crs_sim/src/main/generated-sources/sarl/crs_sim/environment/DefaultQuadTreeNode.java
+++ crs_sim/src/main/generated-sources/sarl/crs_sim/environment/DefaultQuadTreeNode.java
@@ -18,6 +18,8 @@ import org.eclipse.xtext.xbase.lib.Pure;
 public class DefaultQuadTreeNode extends QuadTreeNode<EnvObject, DefaultQuadTreeNode> {
   private Rectangle2d area;
   
+  private QuadTreeNode tree;
+  
   public DefaultQuadTreeNode() {
     super();
   }
@@ -26,8 +28,9 @@ public class DefaultQuadTreeNode extends QuadTreeNode<EnvObject, DefaultQuadTree
    * Construct a node.
    * @param data are the initial user data
    */
-  public DefaultQuadTreeNode(final Collection<EnvObject> data) {
-    super(data);
+  public DefaultQuadTreeNode(final Collection<EnvObject> collec) {
+    throw new Error("Unresolved compilation problems:"
+      + "\nThe method buildTree(DefaultQuadTreeNode, Collection<EnvObject>) is undefined for the type Class<DefaultTreeBuilder>");
   }
   
   public void insert(final EnvObject object) {
@@ -49,5 +52,5 @@ public class DefaultQuadTreeNode extends QuadTreeNode<EnvObject, DefaultQuadTree
   }
   
   @SyntheticMember
-  private static final long serialVersionUID = -1893959734L;
+  private static final long serialVersionUID = -1749294451L;
 }
