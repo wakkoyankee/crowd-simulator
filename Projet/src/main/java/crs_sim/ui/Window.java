db83f3095b43a56b759b54bdf5c22699fae6864e
Twomens <55756182+Twomens@users.noreply.github.com>
2021-05-07 23:53:42 +0200

    ihm vite fait

--- Projet/src/main/java/crs_sim/ui/Window.java
+++ Projet/src/main/java/crs_sim/ui/Window.java
@@ -1,4 +1,5 @@
 package crs_sim.ui;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.util.ArrayList;
@@ -16,7 +17,8 @@ public class Window {
 	public Window() {
 		this._win = new JFrame();
 		this._win.setTitle("CRS simulator");
-		this._win.setSize((int)ParamSimu.mapSizeX, (int)ParamSimu.mapSizeY);
+//		this._win.setSize((int)ParamSimu.mapSizeX, (int)ParamSimu.mapSizeY);
+		this._win.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		this._win.setLocationRelativeTo(null);
 		this._win.setVisible(true);
 		
@@ -49,8 +51,27 @@ public class Window {
             //else circle
             //g.fillOval(c.x, c.y, c.diameter, c.diameter)
             
+            g.setColor(Color.LIGHT_GRAY);
+            g.fillRect((int) ((1960-ParamSimu.mapSizeX)/2),
+            		0,
+            		(int) ParamSimu.mapSizeX,
+            		(int) ParamSimu.mapSizeY);
+            g.setColor(Color.BLACK);
+            g.drawRect((int) ((1960-ParamSimu.mapSizeX)/2),
+            		0,
+            		(int) ParamSimu.mapSizeX,
+            		(int) ParamSimu.mapSizeY);
             
-            g.drawRect(100, 200, 400, 400);
+//            for(Percept body: bodies) {
+//            	if(body.getName() == "Protestor") {
+//            		g.fillOval(0, 0, 10, 10);
+//            	} else if (body.getName() == "Building") {
+//                    g.fillRect(0, 0, 10, 10);
+//            	} else if(body.getName() == "CRS") {
+//            		g.fillOval(0, 0, 10, 10);
+//            	}
+//            }
+            
         }
         
         public void setBodies(ArrayList<Percept> bodies) {
