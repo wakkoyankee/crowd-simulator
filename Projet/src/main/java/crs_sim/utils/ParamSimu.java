db5ec9c8becf5c9530977b49f7cb488c74faa617
wakkoyankee <rothea.hadrien@gmail.com>
2021-05-18 11:28:42 +0200

    correction appel percept

--- Projet/src/main/java/crs_sim/utils/ParamSimu.java
+++ Projet/src/main/java/crs_sim/utils/ParamSimu.java
@@ -25,6 +25,7 @@ public class ParamSimu {
 	public static final int BuildingForce = 1;
 	public static final int CRSForce = 1;
 	public static final int TargetForce = 10;
+	public static final int RadiusPerceptProtestor = 100;
 
 	//objectif neutre
 	//objectif agg
