fc000edf03e052b34aac8ee558e2adaba70f5b67
wakkoyankee <rothea.hadrien@gmail.com>
2021-05-25 14:06:25 +0200

    resolveinf & inf & aggressive beh

--- Projet/src/main/java/crs_sim/utils/ParamSimu.java
+++ Projet/src/main/java/crs_sim/utils/ParamSimu.java
@@ -23,7 +23,7 @@ public class ParamSimu {
 	public static final int CRSForce = 1;
 	public static final int TargetForce = 10;
 	public static final int RadiusPerceptProtestor = 100;
-	public static final int Speed = 10; //raporter la distance de la speed au temps en seconde d'un tic 
+	public static final double Speed = 0.55; //raporter la distance de la speed au temps en seconde d'un tic 1 TICK = 0.5 sec
 	public static final int PanicOnPanic = 5;
 	public static final int RadiusPerceptCrs = 100;
 
