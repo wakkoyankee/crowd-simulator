6fd78c26918b80f79488f86de24e13cf9593fe73
enmur <57959498+enmur@users.noreply.github.com>
2021-06-01 10:24:15 +0200

    meilleur physique mais pas parfait

--- Projet/src/main/java/crs_sim/utils/ParamSimu.java
+++ Projet/src/main/java/crs_sim/utils/ParamSimu.java
@@ -21,7 +21,7 @@ public class ParamSimu {
 	public static final int AgressiveForce = 1;
 	public static final int BuildingForce = 10;
 	public static final int CRSForce = 3;
-	public static final int TargetForce = 10;
+	public static final int TargetForce = 5;
 	public static final int RadiusPerceptProtestor = 100;
 
 	public static final double Speed = 0.4; //raporter la distance de la speed au temps en seconde d'un tic 1 TICK = 0.5 sec
